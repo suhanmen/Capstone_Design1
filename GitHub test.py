aace134ef486ecff96938bef85d70b456abfd1ab
Fermata-YOON <98hsyoon@naver.com>
2023-02-21 00:38:43 +0900

    token push test2

---
+++ GitHub test.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import numpy as np
+
+print('Github push test')
